97f15fa92f1bff767f754cf5863e35ba6ee96dff
刘鹏 <liupeng01@4paradigm.com>
2022-05-16 00:40:51 +0800

    [ADD] add img

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 使用浏览器 cookie 将饭否时间线导出至 Excel 文件。
 
 ```bash
-git clone xxx
-cd xxx
+git clone https://github.com/FingerLiu/fanfou-exporter.git
+cd fanfou-exporter
 pip install -r requirements.txt
 ./fanfou-exporter.py --help
 ./fanfou-exporter.py --homepage "https://fanfou.com/bitcher" --cookie "__utmc=208515845; xxxxx..."
--- fanfou-exporter.py
+++ fanfou-exporter.py
@@ -2,6 +2,7 @@
 
 # export fanfou timeline to csv
 
+import os
 import argparse
 import time
 import requests
@@ -9,19 +10,26 @@ import xlsxwriter
 from bs4 import BeautifulSoup
 import datetime
 import pprint
-
+import random
+from io import BytesIO
 
 default_homepage_url = "https://fanfou.com/bitcher"
 default_cookie = "__utmc=208515845; __utmz=208515845.1648721440.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); uuid=3871ebcd18210725350e.1648721427.5; PHPSESSID=34jnkrgis2st4fg6jmlo514bp0; __utma=208515845.1228288682.1648721440.1651720913.1652595915.14; __utmv=208515845.self; msg=%E4%B8%BA%E4%BB%80%E4%B9%88%E4%B8%80%E4%B8%AA%E5%B0%8F%E5%8A%A8%E7%89%A9%E8%83%BD%E5%90%AC%E6%87%82%E2%80%9C%E6%8A%8A%E7%8B%97%E7%BB%B3%E5%8F%BC%E8%BF%87%E6%9D%A5%E2%80%9D%E5%91%A2%EF%BC%8C%E7%9C%9F%E7%A5%9E%E5%A5%87%E3%80%82; __utmt=1; __utmb=208515845.46.10.1652595915"
-default_filepath = "./fanfou-{}.xlsx".format(datetime.datetime.now().strftime("%Y-%m-%d,_%H:%M:%S"))
 
 parser = argparse.ArgumentParser(description='Export users timeline using your browser cookie.')
 parser.add_argument('--homepage', default=default_homepage_url)
 parser.add_argument('--cookie', default=default_cookie)
-parser.add_argument('--filepath', default=default_filepath)
+parser.add_argument('--filepath', default='')
 args = parser.parse_args()
 
+name = args.homepage.strip('/').split('/')[-1]
 
+if not args.filepath:
+    filepath = "./fanfou-{}-{}.xlsx".format(name, datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"))
+else:
+    filepath = args.filepath
+
+photo_dir = "./{}-photos".format(name)
 
 # 构建 header 和 cookie
 headers = {
@@ -45,14 +53,23 @@ session = requests.Session()
 session.headers = headers
 
 
-with xlsxwriter.Workbook(args.filepath) as workbook:
+with xlsxwriter.Workbook(filepath) as workbook:
     # add worksheet
     worksheet = workbook.add_worksheet()
     row = 0
-    worksheet.write(row, 0, 'content')
-    worksheet.write(row, 1, 'datetime')
-    worksheet.write(row, 2, 'photo')
-
+    worksheet.write(row, 0, 'datetime')
+    worksheet.write(row, 1, 'content')
+    worksheet.write(row, 2, 'link')
+    worksheet.write(row, 3, 'preview')
+    worksheet.write(row, 4, 'photo')
+    worksheet.write(row, 5, 'photo_raw_link')
+    worksheet.set_column(0, 0, 18)
+    worksheet.set_column(1, 1, 120)
+    worksheet.set_column(2, 2, 40)
+    worksheet.set_column(3, 3, 20)
+    worksheet.set_column(4, 4, 50)
+    worksheet.set_column(5, 5, 100)
+    row += 1
     # 请求第一页
     r = session.get(args.homepage)
     if r.status_code != 200:
@@ -79,21 +96,47 @@ with xlsxwriter.Workbook(args.filepath) as workbook:
         data = []
         for s in stream:
             d = dict()
-            d['content'] = s.select_one('.content').text
             d['datetime'] = s.select_one('.time').text
+            d['content'] = s.select_one('.content').text
+            link = s.select_one('.content').select_one('a:not(.photo)')
+            if link:
+                d['link'] = link['href']
             photo = s.select_one('.content').select_one('.photo')
             if photo:
                 d['photo'] = photo['href']
+                d['preview'] = photo.select_one('img')['src']
             data.append(d)
         pprint.pprint(data)
 
-        print("start write page {} to xlsx {}".format(i, args.filepath))
+        print("start write page {} to xlsx {}".format(i, filepath))
         for d in data:
-            worksheet.write(row, 0, d['content'])
-            worksheet.write(row, 1, d['datetime'])
-            worksheet.write(row, 2, d.get('photo'))
+            worksheet.write(row, 0, d['datetime'])
+            worksheet.write(row, 1, d['content'])
+            if d.get('link'):
+                worksheet.write(row, 2, d['link'])
+
+            # get photo data
+            if d.get('photo'):
+                r = session.get(d['photo'])
+                if r.status_code != 200:
+                    print("err fetch {}: {} {}".format(d['photo'], r.reason, r.status_code))
+                    continue
+
+                # save photo
+                os.makedirs(photo_dir, exist_ok=True)
+                img = "{}/{}.jpg".format(photo_dir, d['datetime'])
+                with open(img, "wb") as f:
+                    f.write(r.content)
+                
+                preview = session.get(d['preview'])
+                if preview.status_code != 200:
+                    continue
+                worksheet.insert_image(row, 3, img, {'image_data': BytesIO(preview.content)})
+                worksheet.set_row(row, 100)
+                worksheet.write_url(row, 4, img)
+                worksheet.write(row, 5, d.get('photo'))
             row += 1
-        time.sleep(0.05)
+        time.sleep(0.04 + random.random() * 0.01)
 
     print("succeed.")
 
